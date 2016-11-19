4dc2aa4d5e0d7e91c4214caca43b4a0b3071cdb9
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-11-19 17:09:40 -0200

    Sorveteria

--- Soverteria/Sorverteria.Web/Controllers/HomeController.cs
+++ Soverteria/Sorverteria.Web/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using Sorverteria.Web.Models;
 
 namespace Sorverteria.Web.Controllers
 {
@@ -10,7 +8,29 @@ namespace Sorverteria.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var listaSorvetes = new List<Sorvete>()
+            {
+                new Sorvete
+                {
+                    Sabor = "Morango",
+                    Descricao = "Muito bom!!!",
+                    Foto = "http://www.receitasdemae.com.br/wp-content/uploads/2013/01/sorvete-de-morango.jpg"
+                },
+                 new Sorvete
+                {
+                    Sabor = "Chocolate",
+                    Descricao = "Muito bom!!!",
+                    Foto = "http://s.glbimg.com/po/rc/media/2013/01/03/09_27_31_109_sorvete.jpg"
+                },
+                  new Sorvete
+                {
+                    Sabor = "Abacaxi",
+                    Descricao = "Muito bom!!!",
+                    Foto = "http://www.receitasanamaria.net/wp-content/uploads/2015/01/Receita-Sorvete-Cremoso-de-Abacaxi.jpg"
+                }
+            };
+
+            return View(listaSorvetes);
         }
 
         public ActionResult About()
